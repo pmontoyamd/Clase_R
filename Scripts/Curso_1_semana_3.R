11c2c68463814bfc00cc29d100409e84e9c3a519
pmontoyamd <pmontoya@conceptum.com>
2021-02-28 18:46:44 -0500

    Initial Commit

---
+++ Scripts/Curso_1_semana_3.R
@@ -0,0 +1,11 @@
+# Curso 1: semana 3
+# Version control
+# Git terminology:
+  # Repository - Repo
+  # Commit - save changes - snapshot 
+  # Push: update repository with edits
+  # Pull: updating local version of the repository to the current version
+  # Stage: preparan antes de commit
+  # Branch: ramificaciones del archivo original que trabajo y aún no he hecho push
+  # Merge: unión del branch con el archivo originsl
+  # Conflict: muchos cambios por los usuarios y Git no es capaz de saber cómo unirlos
